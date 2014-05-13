e3b06b6af6039fb58a500e9d0ef70f557ac9c10a
Andrew Appleton <andrew@floatleft.com>
2014-05-13 21:53:56 +0100

    Fix removing styles, indent list items.

--- Notesy/MarkdownTextStorage.m
+++ Notesy/MarkdownTextStorage.m
@@ -11,7 +11,8 @@
 @interface MarkdownTextStorage()
 @property (nonatomic,strong) NSMutableAttributedString *attributedString;
 @property (nonatomic,strong) NSDictionary *attributeDictionary;
-@property (nonatomic,strong) NSDictionary *bodyFont;
+@property (nonatomic,strong) UIFont *bodyFont;
+@property (nonatomic,strong) UIColor *bodyColour;
 @end
 
 @implementation MarkdownTextStorage {
@@ -22,25 +23,20 @@
 - (instancetype) initWithString:(NSString *)str {
     self = [super init];
     if (self) {
-        _bodyFont = @{NSFontAttributeName: [UIFont fontWithName:@"SourceCodePro-Regular" size:17],
-                      NSForegroundColorAttributeName: [UIColor blackColor],
-                      NSUnderlineStyleAttributeName: [NSNumber numberWithInt:NSUnderlineStyleNone]};
         _attributedString = [[NSMutableAttributedString alloc] initWithString:str];
-
-        [self createHighlightPatterns];
     }
     return self;
 }
 
-- (NSString *)string {
+- (NSString *) string {
     return [_attributedString string];
 }
 
-- (NSDictionary *)attributesAtIndex:(NSUInteger)location effectiveRange:(NSRangePointer)range {
+- (NSDictionary *) attributesAtIndex:(NSUInteger)location effectiveRange:(NSRangePointer)range {
     return [_attributedString attributesAtIndex:location effectiveRange:range];
 }
 
-- (void)replaceCharactersInRange:(NSRange)range withString:(NSString*)str {
+- (void) replaceCharactersInRange:(NSRange)range withString:(NSString*)str {
     [self beginEditing];
 
     [_attributedString replaceCharactersInRange:range withString:str];
@@ -49,7 +45,7 @@
     [self endEditing];
 }
 
-- (void)setAttributes:(NSDictionary*)attrs range:(NSRange)range {
+- (void) setAttributes:(NSDictionary*)attrs range:(NSRange)range {
     [self beginEditing];
 
     [_attributedString setAttributes:attrs range:range];
@@ -58,50 +54,25 @@
     [self endEditing];
 }
 
--(void)processEditing {
-    [self performReplacementsForRange:[self editedRange]];
+-(void) processEditing {
     [super processEditing];
+    NSRange paragaphRange = [self.string paragraphRangeForRange:self.editedRange];
+    NSLog(@"%@", [self.string substringWithRange:paragaphRange]);
+
+    [self resetRange:paragaphRange];
+    [self applyStylesToRange:paragaphRange];
 }
 
-- (void)performReplacementsForRange:(NSRange)changedRange {
-    NSRange extendedRange = NSUnionRange(changedRange, [[_attributedString string] lineRangeForRange:NSMakeRange(changedRange.location, 0)]);
-    extendedRange = NSUnionRange(changedRange, [[_attributedString string] lineRangeForRange:NSMakeRange(NSMaxRange(changedRange), 0)]);
-
-    [self applyStylesToRange:extendedRange];
+- (void) resetRange:(NSRange)range {
+    [self addAttribute:NSFontAttributeName value:self.bodyFont range:range];
+    [self addAttribute:NSForegroundColorAttributeName value:self.bodyColour range:range];
+    [self removeAttribute:NSParagraphStyleAttributeName range:range];
 }
 
-- (void)createHighlightPatterns {
-    NSDictionary *boldAttributes = @{NSFontAttributeName: [UIFont fontWithName:@"SourceCodePro-Bold" size:17]};
-    NSDictionary *italicAttributes = @{NSFontAttributeName:[UIFont fontWithName:@"SourceCodePro-Regular" size:17]};
-    NSDictionary *boldItalicAttributes = @{NSFontAttributeName:[UIFont fontWithName:@"SourceCodePro-Bold" size:17]};
-    NSDictionary *codeAttributes = @{NSForegroundColorAttributeName:[UIColor grayColor]};
-    NSDictionary *headerAttributes = boldAttributes;
-
-    NSDictionary *linkAttributes = @{NSForegroundColorAttributeName:[UIColor colorWithRed:0.255 green:0.514 blue:0.769 alpha:1.00]};
-
-    _attributeDictionary = @{
-                             @"(#+.*)":headerAttributes,
-                             @"[a-zA-Z0-9\t\n ./<>?;:\\\"'`!@#$%^&*()[]{}_+=|\\-]":_bodyFont,
-                             @"\\**(?:^|[^*])(\\*\\*(\\w+(\\s\\w+)*)\\*\\*)":boldAttributes,
-                             @"\\**(?:^|[^*])(\\*(\\w+(\\s\\w+)*)\\*)":italicAttributes,
-                             @"(\\*\\*\\*\\w+(\\s\\w+)*\\*\\*\\*)":boldItalicAttributes,
-                             @"(`\\w+(\\s\\w+)*`)":codeAttributes,
-                             @"(```\n([\\s\n\\d\\w[/[\\.,-\\/#!?@$%\\^&\\*;:|{}<>+=\\-'_~()\\\"\\[\\]\\\\]/]]*)\n```)":codeAttributes,
-                             @"(\\[\\w+(\\s\\w+)*\\]\\(\\w+\\w[/[\\.,-\\/#!?@$%\\^&\\*;:|{}<>+=\\-'_~()\\\"\\[\\]\\\\]/ \\w+]*\\))":linkAttributes
-                             };
-}
-
--(void)update {
-    [self createHighlightPatterns];
-    [self addAttributes:_bodyFont range:NSMakeRange(0, self.length)];
-    [self applyStylesToRange:NSMakeRange(0, self.length)];
-}
-
-- (void)applyStylesToRange:(NSRange)searchRange {
-    for (NSString *key in _attributeDictionary) {
+- (void) applyStylesToRange:(NSRange)searchRange {
+    for (NSString *key in self.attributeDictionary) {
         NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:key options:0 error:nil];
-
-        NSDictionary *attributes = _attributeDictionary[key];
+        NSDictionary *attributes = self.attributeDictionary[key];
 
         [regex enumerateMatchesInString:[_attributedString string]
                                 options:0
@@ -110,12 +81,52 @@
          ^(NSTextCheckingResult *match, NSMatchingFlags flags, BOOL *stop) {
              NSRange matchRange = [match rangeAtIndex:1];
              [self addAttributes:attributes range:matchRange];
-
-             if (NSMaxRange(matchRange)+1 < self.length) {
-                 [self addAttributes:_bodyFont range:NSMakeRange(NSMaxRange(matchRange)+1, 1)];
-             }
         }];
     }
+}
+
+#pragma mark - Getters
+
+- (NSDictionary *)attributeDictionary {
+    if (!_attributeDictionary) {
+        NSDictionary *boldAttributes = @{NSFontAttributeName: [UIFont fontWithName:@"SourceCodePro-Bold" size:17]};
+        NSDictionary *italicAttributes = @{NSFontAttributeName: [UIFont fontWithName:@"SourceCodePro-Regular" size:17]};
+        NSDictionary *boldItalicAttributes = @{NSFontAttributeName: [UIFont fontWithName:@"SourceCodePro-Bold" size:17]};
+        NSDictionary *codeAttributes = @{NSForegroundColorAttributeName: [UIColor grayColor]};
+        NSDictionary *headerAttributes = boldAttributes;
+        NSDictionary *linkAttributes = @{NSForegroundColorAttributeName: [UIColor colorWithRed:0.255 green:0.514 blue:0.769 alpha:1.00]};
+
+        NSMutableParagraphStyle *listParagraph = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+        listParagraph.headIndent = 20.0;
+        NSDictionary *listAttributes = @{NSParagraphStyleAttributeName: listParagraph};
+
+        _attributeDictionary = @{
+            @"(#+.*)": headerAttributes,
+//            @"[a-zA-Z0-9\t\n ./<>?;:\\\"'`!@#$%^&*()[]{}_+=|\\-]": self.bodyFont,
+            @"\\**(?:^|[^*])(\\*\\*(\\w+(\\s\\w+)*)\\*\\*)": boldAttributes,
+            @"\\**(?:^|[^*])(\\*(\\w+(\\s\\w+)*)\\*)": italicAttributes,
+            @"(\\*\\*\\*\\w+(\\s\\w+)*\\*\\*\\*)": boldItalicAttributes,
+            @"(`\\w+(\\s\\w+)*`)": codeAttributes,
+            @"(```\n([\\s\n\\d\\w[/[\\.,-\\/#!?@$%\\^&\\*;:|{}<>+=\\-'_~()\\\"\\[\\]\\\\]/]]*)\n```)": codeAttributes,
+            @"(\\[\\w+(\\s\\w+)*\\]\\(\\w+\\w[/[\\.,-\\/#!?@$%\\^&\\*;:|{}<>+=\\-'_~()\\\"\\[\\]\\\\]/ \\w+]*\\))": linkAttributes,
+            @"(\\*|\\-|\\+)(.*)": listAttributes
+        };
+    }
+    return _attributeDictionary;
+}
+
+- (UIFont *) bodyFont {
+    if (!_bodyFont) {
+        _bodyFont = [UIFont fontWithName:@"SourceCodePro-Regular" size:17];
+    }
+    return _bodyFont;
+}
+
+- (UIColor *) bodyColour {
+    if (!_bodyColour) {
+        _bodyColour = [UIColor colorWithRed:0/255.0f green:4/255.0f blue:68/255.0f alpha:1.0f];
+    }
+    return _bodyColour;
 }
 
 @end
