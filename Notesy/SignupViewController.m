ae297632fb345f371dbb23d126f3fb007619251c
Andrew Appleton <andrew@floatleft.com>
2014-05-15 21:32:25 +0100

    Empty signup controller

---
+++ Notesy/SignupViewController.m
@@ -0,0 +1,35 @@
+//
+//  SignupViewController.m
+//  Notesy
+//
+//  Created by Andy Appleton on 15/05/2014.
+//  Copyright (c) 2014 Notesy.co. All rights reserved.
+//
+
+#import "SignupViewController.h"
+
+@interface SignupViewController ()
+
+@end
+
+@implementation SignupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
